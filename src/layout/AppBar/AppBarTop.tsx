15b8f229a88b3467c6a9502c23557c251a95e160
tiendunggg99 <tiendung_ms@outlook.com>
2022-09-09 19:09:53 +0700

    add Avatar

--- src/layout/AppBar/AppBarTop.tsx
+++ src/layout/AppBar/AppBarTop.tsx
@@ -4,6 +4,8 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar, Box } from "@mui/material";
+import "./AppBarTop.scss";
 
 const drawerWidth = 240;
 interface AppBarProps extends MuiAppBarProps {
@@ -47,6 +49,12 @@ export default function AppBarTop(props: any) {
         <Typography variant="h6" noWrap component="div">
           Mini variant drawer
         </Typography>
+        <Box display="flex" flexGrow={1}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://mui.com/static/images/avatar/1.jpg"
+          />
+        </Box>
       </Toolbar>
     </AppBar>
   );
